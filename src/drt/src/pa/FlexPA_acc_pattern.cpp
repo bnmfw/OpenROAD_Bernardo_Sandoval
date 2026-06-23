cd61753b5945624b1620f11489b4c4017a852cfb
Bernardo Borges Sandoval <39677852+bnmfw@users.noreply.github.com>
2026-06-23 14:22:27 -0300

    Update FlexPA_acc_pattern.cpp
    fixed typo
    
    Signed-off-by: Bernardo Borges Sandoval <39677852+bnmfw@users.noreply.github.com>

--- src/drt/src/pa/FlexPA_acc_pattern.cpp
+++ src/drt/src/pa/FlexPA_acc_pattern.cpp
@@ -456,12 +456,12 @@ int FlexPA::getEdgeCost(
                                 curr_acc_point_idx,
                                 max_access_point_size);
 
-  // edge know to be a violation
+  // edge know to have a violation
   if (vio_edges[edge_idx] == 1) {
     return violation_cost;
   }
 
-  // edge not known to be a violation of not
+  // edge not known to have a violation or not
   if (vio_edges[edge_idx] != -1) {
     odb::dbTransform xform = unique_inst->getNoRotationTransform();
     // check DRC
